096d5d554ee8cd08db7a823a1b737d30ebcd2fc7
Bharath <bharath.alla23@gmail.com>
2026-05-15 12:12:50 +0530

    Initial Commit

---
+++ iOSPOCApp/iOSPOCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPOCAppApp.swift
+//  iOSPOCApp
+//
+//  Created by Alla Bharath on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSPOCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
